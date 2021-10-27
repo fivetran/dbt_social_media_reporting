73031ea1c99c3b3e23dce6173dfbdeefbffce675
Dylan Baker <dylan@dbanalytics.co>
2021-10-27 12:11:48 -0400

    remove _prep

--- models/social_media_reporting__rollup_report.sql
+++ models/social_media_reporting__rollup_report.sql
@@ -2,10 +2,10 @@ with unioned as (
 
     {{ dbt_utils.union_relations(
         [
-            ref('social_media_reporting__facebook_posts_reporting_prep'),
-            ref('social_media_reporting__linkedin_posts_reporting_prep'),
-            ref('social_media_reporting__twitter_posts_reporting_prep'),
-            ref('social_media_reporting__instagram_posts_reporting_prep')
+            ref('social_media_reporting__facebook_posts_reporting'),
+            ref('social_media_reporting__linkedin_posts_reporting'),
+            ref('social_media_reporting__twitter_posts_reporting'),
+            ref('social_media_reporting__instagram_posts_reporting')
         ]
     )}}
 
