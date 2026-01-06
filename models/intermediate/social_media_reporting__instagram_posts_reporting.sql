5dc519a19630553b6d12d3bebd9e91d73e5207e0
Savage Fivetran <sarah.savage@fivetran.com>
2026-01-06 14:39:59 -0800

    remove_vide_photo_form_sum

--- models/intermediate/social_media_reporting__instagram_posts_reporting.sql
+++ models/intermediate/social_media_reporting__instagram_posts_reporting.sql
@@ -18,21 +18,11 @@ with report as (
         'instagram' as platform,
         coalesce(sum(comment_count),0) as comments,
         coalesce(sum(like_count),0) as likes,
-   sum(
-           case
-               -- For carousel albums, use carousel-specific views (not video_photo which duplicates)
-               when carousel_album_views is not null
-                   then coalesce(carousel_album_views, carousel_album_impressions, 0)
-               -- For reels, use reel views (not video_photo which duplicates)
-               when reel_views is not null
-                   then reel_views
-               -- For stories, use story views
-               when story_views is not null
-                   then coalesce(story_views, story_impressions, 0)
-               -- For regular video/image posts, use video_photo views
-               else coalesce(video_photo_views, video_photo_impressions, 0)
-           end
-       ) as impressions -- *_impressions are DEPRECATED, to be removed at a later time
+sum(
+    coalesce(carousel_album_views, carousel_album_impressions, 0)
+  + coalesce(story_views, story_impressions, 0)
+  + coalesce(reel_views, 0)
+) as impressions-- *_impressions are DEPRECATED, to be removed at a later time
     from report
     {{ dbt_utils.group_by(8) }}
 
