77fb940e0ddad12f13923bf884d4f900592ae13e
Jamie Rodriguez <jamie.rodriguez@fivetran.com>
2026-08-10 15:10:21 -0400

    use dev branch of Youtube

--- macros/duckdb__json_parse.sql
+++
@@ -1,8 +0,0 @@
-{% macro duckdb__json_parse(string, string_path) %}
-
-    json_extract_string(
-        {{ string }},
-        '$.{%- for s in string_path -%}{{ s }}{%- if not loop.last -%}.{%- endif -%}{%- endfor -%}'
-    )
-
-{% endmacro %}
